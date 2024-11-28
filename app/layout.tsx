336c7da5fd5f822aea66ed1d37b22f9fce5c4eeb
Martin Lupa <marlupa@dfds.com>
2024-11-28 21:46:40 +0100

    feat: add initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,60 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+import LocalFont from 'next/font/local';
+import { Metadata } from 'next';
+import { Analytics } from './components/analytics';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'martinlupa.com',
+    template: '%s | martinlupa.com',
+  },
+  description:
+    'Full-Stack Developer with expertise in JavaScript, TypeScript, Go, React, Next.js, Node.js, and AWS. Skilled in building scalable, secure applications, DevOps pipelines, and cloud infrastructure using Docker, Terraform, and CI/CD.',
+  openGraph: {
+    title: 'martinlupa.com',
+    description: 'Full-Stack Developer: React.js, Next.js, Node.js, Go, AWS, IaC)',
+    url: 'https://martinlupa.com',
+    siteName: 'martinlupa.com',
+    locale: 'en-US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    shortcut: '/favicon.png',
+  },
+};
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+const calSans = LocalFont({
+  src: '../public/fonts/CalSans-SemiBold.ttf',
+  variable: '--font-calsans',
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(' ')}>
+      <head>
+        <Analytics />
+      </head>
+      <body
+        className={`bg-black ${process.env.NODE_ENV === 'development' ? 'debug-screens' : undefined}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import React from 'react';
+import Particles from './components/particles';
+
+const navigation = [
+  { name: 'Projects', href: '/projects' },
+  { name: 'Contact', href: '/contact' },
+];
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="my-16 animate-fade-in">
+        <ul className="flex items-center justify-center gap-4">
+          {navigation.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="text-sm text-zinc-500 duration-500 hover:text-zinc-300"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </ul>
+      </nav>
+      <div className="animate-glow hidden h-px w-screen animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
+      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
+      <h1 className="text-edge-outline z-10 animate-title cursor-default whitespace-nowrap bg-white bg-clip-text px-0.5 py-3.5 font-display text-4xl text-transparent duration-1000 sm:text-6xl md:text-9xl">
+        Martin Lupa
+      </h1>
+
+      <div className="animate-glow hidden h-px w-screen animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
+      <div className="my-16 animate-fade-in text-center">
+        <h2 className="text-sm text-zinc-500">
+          Full-Stack Developer: React.js, Next.js, Node.js, Go, AWS, IaC
+        </h2>
+      </div>
+    </div>
+  );
+}
