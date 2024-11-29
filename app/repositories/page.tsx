9ffe000a35e3f3a3d1b23f78d618a0884426dbd8
Martin Lupa <marlupa@dfds.com>
2024-11-29 21:51:26 +0100

    refactor: update colors

--- app/repositories/page.tsx
+++ app/repositories/page.tsx
@@ -21,13 +21,9 @@ export default async function Projects() {
   const repositoriesData = await fetchAllRepos()
 
   const pinnedColors = [
-    'bg-blue-00',
+    'bg-blue-500',
     'bg-green-500',
     'bg-red-500',
-    'bg-yellow-500',
-    'bg-purple-500',
-    'bg-pink-500',
-    'bg-teal-500',
   ]
 
   return (
