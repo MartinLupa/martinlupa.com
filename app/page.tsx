097cd00631a66b16f92cc8133dce006e37cd2e17
Martin Lupa <marlupa@dfds.com>
2024-11-29 12:47:47 +0100

    fix: animation not working on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -23,13 +23,13 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-      <div className="animate-glow hidden h-px w-screen animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
+      <div className="animate-glow h-px w-screen animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={2000} />
       <h1 className="text-edge-outline z-10 animate-title cursor-default whitespace-nowrap bg-white bg-clip-text px-0.5 py-3.5 font-display text-4xl text-transparent duration-1000 sm:text-6xl md:text-9xl">
         Martin Lupa
       </h1>
 
-      <div className="animate-glow hidden h-px w-screen animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
+      <div className="animate-glow h-px w-screen animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 animate-fade-in text-center">
         <h2 className="text-lg text-zinc-500">
           Full-Stack Developer: React.js, Next.js, Node.js, Go, AWS, IaC, CI/CD
