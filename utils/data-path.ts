375f6a2f4a487a1fa2905b6d231d1dfad5d5c44e
Martin Lupa <marlupa@dfds.com>
2024-12-08 22:02:25 +0100

    fix: data path

--- utils/data-path.ts
+++ utils/data-path.ts
@@ -1,4 +1,4 @@
 export const repositoriesDataPath =
   process.env.VERCEL_ENV === 'production'
-    ? `${process.env.NEXT_PUBLIC_BASE_URL}/repositories.json`
+    ? `${process.env.NEXT_PUBLIC_BASE_URL}/public/repositories.json`
     : './public/repositories.json'
